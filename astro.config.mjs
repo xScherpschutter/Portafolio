02a66749898799b943042c1eb151538673674ada
Willian <konradlisthrop@gmail.com>
2025-09-07 18:48:06 -0500

    feat: Github Workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: "https://xscherpschutter.github.io/Portafolio",
-  base: "/portfolio/",
+  base: "/Portafolio/",
   integrations: [tailwind()],
 });
